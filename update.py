f53d75705f3523b33b369145d6a8a234fd3018f7
devops-itcs <83263728+devops-itcs@users.noreply.github.com>
2022-02-08 20:03:00 +0700

    Update update.py

--- update.py
+++ update.py
@@ -3,28 +3,37 @@
 import paramiko
 import psycopg2
 
-server = "10.10.154.101"
+server1 = "10.10.154.100"
+server2 = "10.10.154.101"
 
-conn = psycopg2.connect(
-   database="opensips", user='postgres', password='postgres', host=server, port= '5432'
-)
-conn.autocommit = True
-cursor = conn.cursor()
-cursor.execute("truncate load_balancer RESTART IDENTITY CASCADE;")
+for server in (server1,server2):
+	try:
 
-file = open("gsm.csv").readlines()
-for line in file:
-	if line != "":
-		data = line.strip().split(',')
-		sql = (int(data[0]),data[1].replace('"',''),data[2].replace('"',''),int(data[3]),data[4].replace('"',''))
-		cursor.execute('''
-			INSERT INTO load_balancer(group_id,dst_uri,resources,probe_mode,description) \
-			VALUES {}'''.format(sql))
+		conn = psycopg2.connect(
+			 database="opensips", user='postgres', password='postgres', host=server, port= '5432'
+		)
+		conn.autocommit = True
+		cursor = conn.cursor()
+		cursor.execute("truncate load_balancer RESTART IDENTITY CASCADE;")
 
-conn.commit()
-conn.close()
+		file = open("gsm.csv").readlines()
+		for line in file:
+			if line != "":
+				data = line.strip().split(',')
+				sql = (int(data[0]),data[1].replace('"',''),data[2].replace('"',''),int(data[3]),data[4].replace('"',''))
+				cursor.execute('''
+					INSERT INTO load_balancer(group_id,dst_uri,resources,probe_mode,description) \
+					VALUES {}'''.format(sql))
+
+		conn.commit()
+		conn.close()
+	
+	except: pass
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(server, username="root", password="Pls@1234!")
-ssh.exec_command("opensipsctl fifo lb_reload")
+for server in (server1,server2):
+	try:
+		ssh.connect(server, username="root", password="Pls@1234!")
+		stdin,stdout,stderr = ssh.exec_command("opensipsctl fifo lb_reload")
+	except: pass

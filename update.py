d8aa2c7d726ce4f9a1e0ba78328456c7c3c48878
devops-itcs <83263728+devops-itcs@users.noreply.github.com>
2022-02-08 20:57:10 +0700

    Update update.py

--- update.py
+++ update.py
@@ -3,28 +3,46 @@
 import paramiko
 import psycopg2
 
-server = "10.10.154.101"
+server1 = "10.10.154.100"
+server2 = "10.10.154.101"
 
-conn = psycopg2.connect(
-   database="opensips", user='postgres', password='postgres', host=server, port= '5432'
-)
-conn.autocommit = True
-cursor = conn.cursor()
-cursor.execute("truncate load_balancer RESTART IDENTITY CASCADE;")
+def gen_list():
+  sql_list = []
+  for line in open("gsm.csv").readlines():
+    if line != "":
+      if len(line) > 1:
+        data = line.strip().split(',')
+        sql = (int(data[0]),data[1].replace('"',''),data[2].replace('"',''),int(data[3]),data[4].replace('"',''))
+        sql_list.append('''\
+INSERT INTO load_balancer(group_id,dst_uri,resources,probe_mode,description) \
+VALUES {}'''.format(sql))
+  return sql_list
 
-file = open("gsm.csv").readlines()
-for line in file:
-	if line != "":
-		data = line.strip().split(',')
-		sql = (int(data[0]),data[1].replace('"',''),data[2].replace('"',''),int(data[3]),data[4].replace('"',''))
-		cursor.execute('''
-			INSERT INTO load_balancer(group_id,dst_uri,resources,probe_mode,description) \
-			VALUES {}'''.format(sql))
 
-conn.commit()
-conn.close()
+for server in (server1,server2):
+  try:
+    conn = psycopg2.connect(
+      database="opensips", 
+      user='opensips', 
+      password='opensipsrw', 
+      host=server, 
+      port= '5432'
+    )
+    conn.autocommit = True
+    cursor = conn.cursor()
+
+    for sql in gen_list():
+      cursor.execute(sql)
+
+    conn.commit()
+    conn.close()
+  
+  except: pass
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(server, username="root", password="Pls@1234!")
-ssh.exec_command("opensipsctl fifo lb_reload")
+for server in (server1,server2):
+  try:
+    ssh.connect(server, username="root", password="Pls@1234!")
+    stdin,stdout,stderr = ssh.exec_command("opensipsctl fifo lb_reload")
+  except: pass

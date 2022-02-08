38abdfc5327b4312915d86f6b270c02582673b42
devops-itcs <83263728+devops-itcs@users.noreply.github.com>
2022-02-08 20:11:02 +0700

    Update update.py

--- update.py
+++ update.py
@@ -6,6 +6,18 @@ import psycopg2
 server1 = "10.10.154.100"
 server2 = "10.10.154.101"
 
+def gen_list():
+	sql_list = []
+	file = open("gsm.csv").readlines()
+	for line in file:
+		if line != "":
+			data = line.strip().split(',')
+			sql_list.append(int(data[0]),data[1].replace('"',''),data[2].replace('"',''),int(data[3]),data[4].replace('"',''))
+# 				cursor.execute('''
+# 					INSERT INTO load_balancer(group_id,dst_uri,resources,probe_mode,description) \
+# 					VALUES {}'''.format(sql))
+  return sql_list
+
 for server in (server1,server2):
 	try:
 
@@ -16,14 +28,10 @@ for server in (server1,server2):
 		cursor = conn.cursor()
 		cursor.execute("truncate load_balancer RESTART IDENTITY CASCADE;")
 
-		file = open("gsm.csv").readlines()
-		for line in file:
-			if line != "":
-				data = line.strip().split(',')
-				sql = (int(data[0]),data[1].replace('"',''),data[2].replace('"',''),int(data[3]),data[4].replace('"',''))
-				cursor.execute('''
-					INSERT INTO load_balancer(group_id,dst_uri,resources,probe_mode,description) \
-					VALUES {}'''.format(sql))
+		for sql in gen_list():
+			cursor.execute('''
+ 					INSERT INTO load_balancer(group_id,dst_uri,resources,probe_mode,description) \
+ 					VALUES {}'''.format(sql))
 
 		conn.commit()
 		conn.close()
